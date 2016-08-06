e3ca0ab893160ba63912949f985132f8ecbfe7bd
mohini16 <mohiniudaypatil@gmail.com>
2016-08-06 16:34:15 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,30 @@
+#print("mohini")
+#if 2>3:
+#	print("it works")
+#else:
+#	print("it doesnt")
+#	name="mohini"
+#def hi(name):
+#	if name=="moo":
+#		print("moo")
+#	elif name=="nfkj":
+#		print("DSG")
+#	else:
+#		print("mohini")
+#def hi():
+#	print("hi")
+#	print("how r u")
+	
+#hi("moo")
+
+list=["mohini","sky","henny"]
+def hi(name):
+	print('hi' + name +'!')
+	
+for name in list:
+	hi(name)
+	print('next girl')
+
+i=[1,2,3,4,5,6,7,8]
+for i in range(1,6):
+	print(i)
